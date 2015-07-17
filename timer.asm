1b42e9018405fa30a499e47791945639a7b4454f
jennimiao <shuanghp@163.com>
2015-07-17 19:30:21 +0800

    lsn rewrote the timer.asm

--- timer.asm
+++ timer.asm
@@ -3,32 +3,32 @@
 	j	inter	#zhongduan
 	jr	$26	#yichang
 	
-reset:	lui	$a0,	0x4000
-	addiu	$a0,	$a0,	0x0008
+reset:	lui	$a0,	16384
+	addiu	$a0,	$a0,	8
 	sw	$zero,	0($a0)	#TCON = 0
-	lui	$t1,	0xffff
-	addiu	$t1,	$t1,	0x3caf	#TH = 0xFFFF3CAF
-	lui	$t2,	0xffff
-	addiu	$t2,	$t2,	0xffff	#TL = 0xFFFFFFFF
+	lui	$t1,	-1
+	addiu	$t1,	$t1,	15535	#TH = 0xFFFF3CAF
+	lui	$t2,	-1
+	addiu	$t2,	$t2,	-1	#TL = 0xFFFFFFFF
 	addi	$t0,	$zero,	3
 	sw	$t0,	0($a0)	#TCON = 3
 
 inter:
 	lw	$t0,	0($a0)
-	andi	$t0,	$t0,	0xfffffff9	#TCON &= 0xfffffff9
+	andi	$t0,	$t0,	-7	#TCON &= 0xfffffff9
 	
-reset:	lui	$a0,	0x4000
-	addiu	$a0,	$a0,	0x0008
+reset:	lui	$a0,	16384
+	addiu	$a0,	$a0,	8
 	sw	$zero,	0($a0)	#TCON = 0
-	lui	$t1,	0xffff
-	addiu	$t1,	$t1,	0x3caf	#TH = 0xFFFF3CAF
-	lui	$t2,	0xffff
-	addiu	$t2,	$t2,	0xffff	#TL = 0xFFFFFFFF
+	lui	$t1,	-1
+	addiu	$t1,	$t1,	15535	#TH = 0xFFFF3CAF
+	lui	$t2,	-1
+	addiu	$t2,	$t2,	-1	#TL = 0xFFFFFFFF
 	addi	$t0,	$zero,	3
 	sw	$t0,	0($a0)	#TCON = 3
 	
-	lui	$a1,	0x4000
-	addiu	$a1,	$a1,	0x0014
+	lui	$a1,	16384
+	addiu	$a1,	$a1,	20
 	
 	
 ###############################################################
@@ -38,18 +38,18 @@ main:	#li 	$v0,	5	#the greatest divider begins
 	#li	$v0,	5
 	#syscall
 	#add	$s1,	$v0,	0	#$s1 = n
-	lui	$t2,	0x4000
-	addiu	$t2,	$t2,	0x001c
+	lui	$t2,	16384
+	addiu	$t2,	$t2,	28
 	lw	$t7,	0($t2)
-	andi	$t7,	$t7,	0x000000ff
+	andi	$t7,	$t7,	255
 	lw	$s0,	1($t2)
-	andi	$s0,	$s0,	0x000000ff
+	andi	$s0,	$s0,	255
 	sll	$t7,	$t7,	8
 	addu	$s0,	$s0,	$t7	#(t7 << 8) + s0 = s0
 	lw	$t7,	2($t2)
-	andi	$t7,	$t7,	0x000000ff
+	andi	$t7,	$t7,	255
 	lw	$s1,	3($t2)
-	andi	$s1,	$s1,	0x000000ff
+	andi	$s1,	$s1,	255
 	sll	$t7,	$t7,	8
 	addu	$s1,	$s1,	$t7	#(t7 << 8) + s1 = s1
 
@@ -60,8 +60,8 @@ main:	#li 	$v0,	5	#the greatest divider begins
 test:	bne	$s1,	$zero,	loop	#if (n != 0) zhanzhuanxiangchu
 	addi	$a3,	$s0,	0	#cout << a3
 	addiu	$t2,	$t2,	0
-	lui	$t2,	0x4000
-	addiu	$t2,	$t2,	0x0018
+	lui	$t2,	16384
+	addiu	$t2,	$t2,	24
 	move	$t3,	$a3
 	sw	$t3,	0($t2)
 	srl	$t3,	$t3,	8	#t3 >> 8
@@ -92,5 +92,5 @@ loop1:	sub	$t1,	$t1,	$s1
 
 endinter:
 	lw	$t0,	0($a0)
-	ori	$t0,	$t0,	0x00000002
+	ori	$t0,	$t0,	2
 	jr	$26
